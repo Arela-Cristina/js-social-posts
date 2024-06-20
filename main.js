09dee937e1c1b078437ca43bbc6ff0e2a8bf8ded
Arela-Cristina <instagrandine@gmail.com>
2024-06-21 01:15:41 +0200

    Add event listener structure and conditions.
    Created an Array just whit id which has been clicked.

--- main.js
+++ main.js
@@ -59,33 +59,86 @@ const posts = [
 //chiamiamo il container principale id container
 let container = document.getElementById('container');
 
-//chiamiamo il header
-const header = document.getElementsByClassName('post__header');
-
-
 //Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 
 posts.forEach(containerElement => {
-    container.innerHTML += `<div class='post'> 
-                                <div class="post__header>
+    container.innerHTML += `<div class="post">
+                                <div class="post__header">
                                     <div class="post-meta">
                                         <div class="post-meta__icon">
-                                            <img class="profile-pic" src=${posts.author.image}>
+                                            <img class="profile-pic" src="${containerElement.author.image}">
                                         </div>
                                         <div class="post-meta__data">
-                                            <div class="post-meta__author">${posts.author}</div>
-                                             <div class="post-meta__time">${posts.created}</div>
+                                            <div class="post-meta__author">${containerElement.author.name}</div>
+                                            <div class="post-meta__time">${containerElement.created}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="post__text">${containerElement.content}
+                                </div>
+                                <div class="post__image">
+                                    <img src="${containerElement.media}" alt="">
+                                </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button"  data-postid="1">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter"> Piace a 
+                                            <b id="like-counter-${containerElement.id}" class="js-likes-counter">${containerElement.likes}</b> persone
                                         </div>
                                     </div>
                                 </div>
                             </div>`;
-    
+    console.log('Sono il tuo consolelog', containerElement);
 });
 
 
-//prendiamo profile pic
-// let [profilePic] = posts;
-// const { image } = profilePic;
 
-// console.log('Sono profile pic', profilePic);
+
+
+//chiamiamo il button
+const likeButtons = document.querySelectorAll('a.like-button');
+console.log('Sono il mi piace', likeButtons);
+
+//creiamo un arrayId donde ci andremmo a salvare gli solamente gli ID che sono stati clickati, e questo evento lo aggiungeremo dopo con addEventListener.
+let arrayId = [];
+
+//creo il evento add event listener, ci aggiungo likeButton, e poi index, que fa in modo di avere un indice per ogni like Button ed associarlo ad ogni numero di like rispettivo.
+likeButtons.forEach((likeButton, index) => {
+    likeButton.addEventListener('click', function () {
+
+        //salviamo il valore di index in una variabile id perche, rapresentara ID del notro array posts, ad ogni iterazione Id prendera i valori 0, 1, 2 , 3 , 4 , che correspondono ai valori di ogni id del nostro array posts , e poi  dobbiamo...
+        let id = index + 1;
+
+        //...richiamiammo gli ID del DOM e salviamo nuovamente il valore di index (que come sappiamo e index = id) in una variabile likes, per posteriormente utilizarla per aggiungere ed restare i likes ogni volta che si clicka sul mi piace che e 'likeButton 
+        let likes = document.getElementById(`like-counter-${id}`);
+
+        //Le condizioni, se mi piace(likeButton) contiene la classe 'like-button--liked' 
+        if (likeButton.classList.contains('like-button--liked')) {
+            //1. rimuovo la clase
+            likeButton.classList.remove('like-button--liked');
+            //2. ed resto di uno il numero di likes(perche ho tolto il click). likes.InnerHTML sarebbe il numero di likesnel DOM. 
+            likes.innerHTML = parseInt(likes.innerHTML) - 1;
+            //3. ed tolgo il elemento del arrayId, perche ormai,non appartiene piu li, visto che abbiamo tolto il click ed e diminuito di uno. La condizione per essere nel array e che sia stato clickato y sia aumentato di uno. Per questo utilizziamo filter, che filtra gli id che sono stati sclickati
+            arrayId = arrayId.filter(item => item !== id);
+
+        //Altrimenti 
+        } else {
+            //1. Se non e clickato il mi piace(likeButton) aggiungo la classe 'like-button--liked'
+            likeButton.classList.add('like-button--liked');
+             //2. ed incremento di uno il numero di likes(perche ho fatto  click). likes.InnerHTML sarebbe il numero di likes nel DOM. 
+            likes.innerHTML = parseInt(likes.innerHTML) + 1;
+            //... e poi dobbiamo creare un array con gli ID che sono stati clickati
+            arrayId.push(id);
+        }
+
+        console.log(arrayId);
+    });
+});
+
+
 
